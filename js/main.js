5496a46a62bfd3e181301efd48ccc24f33ffaff9
Daniel Hackney <corncob567dh@gmail.com>
2022-10-11 12:29:53 -0400

    Removing some unnecessary JS. Pausing autoclock when simulation starts.

--- js/main.js
+++ js/main.js
@@ -1,63 +1,55 @@
 const MINTEMP = 35;
 const MAXTEMP = 99;
+let isSimulationRunning = false;
 
-(function(){
-  let inputContainer = document.querySelector(".input-container");
-  let input = inputContainer.firstElementChild.nextElementSibling;
-  let minus = inputContainer.firstElementChild;
-  let plus = inputContainer.lastElementChild;
+let inputContainer = document.querySelector(".input-container");
+inputContainer.addEventListener("click", changeTemp);
+let input = inputContainer.firstElementChild.nextElementSibling;
+let minus = inputContainer.firstElementChild;
+let plus = inputContainer.lastElementChild;
 
-  function changeTemp(e) {
-    let temp = +input.value.substring(0, input.value.indexOf(' '));
-    let unit = ' ' + input.value.substring(input.value.indexOf(' ') + 1); // The unit for the temperature
-    if(e.target == minus && temp > MINTEMP) {
-      temp--;
-      input.value = temp + unit;
-    } else if(e.target == plus && temp < MAXTEMP) {
-      temp++;
-      input.value = temp + unit;
-    }
-    if(temp == MINTEMP){
-      minus.disabled = true;
-    }
-    else if(temp == MAXTEMP){
-      plus.disabled = true;
-    }
-    else{
-      plus.disabled = false;
-      minus.disabled = false;
-    }
+function changeTemp(e) {
+  let temp = +input.value.substring(0, input.value.indexOf(' '));
+  let unit = ' ' + input.value.substring(input.value.indexOf(' ') + 1); // The unit for the temperature
+  if(e.target == minus && temp > MINTEMP) {
+    temp--;
+    input.value = temp + unit;
+  } else if(e.target == plus && temp < MAXTEMP) {
+    temp++;
+    input.value = temp + unit;
   }
-
-  inputContainer.addEventListener("click", changeTemp);
-})();
-
-document.getElementById("waterTemp").addEventListener("input", registerChangeTemp);
-document.getElementById("waterTemp").boxId = 1
-function registerChangeTemp(evt) {
-  let boxId = evt.currentTarget.boxId
-  changeTemp(boxId, this.value);
-}
-
-function changeTemp(boxId, newTemp){
-  document.getElementById("box" + boxId).innerHTML = "Box " + boxId + ": " + newTemp + "F";
+  if(temp == MINTEMP){
+    minus.disabled = true;
+  }
+  else if(temp == MAXTEMP){
+    plus.disabled = true;
+  }
+  else{
+    plus.disabled = false;
+    minus.disabled = false;
+  }
 }
 
 updateClock() // Run this on load so the user sees the date/time immediately without having to wait the first second for it to update.
 setInterval(updateClock, 1000);
 function updateClock() {
-  let date = new Date();
-  document.getElementsByClassName("datetimeDisplay")[0].innerHTML =
-  date.toLocaleDateString([], { month: '2-digit', day: 'numeric', year: 'numeric', hour: "numeric", minute: "2-digit" });
+  if(isSimulationRunning == false){
+    let date = new Date();
+    document.getElementsByClassName("datetimeDisplay")[0].innerHTML =
+    date.toLocaleDateString([], { month: '2-digit', day: 'numeric', year: 'numeric', hour: "numeric", minute: "2-digit" });
+  }
 }
 
-let isSimulationRunning = false
 function startSimulation(){
   if (isSimulationRunning == false){
     isSimulationRunning = true
     let simStartButton = document.getElementById("simStart");
     simStartButton.innerHTML = "Simulation Running"
-    setInterval(simulationStep, 1000)
+    simStartButton.disabled = 'true';
+    let simStartDate = new Date(Date.UTC(2022, 11, 1, 5, 0, 0));
+    let options = { month: '2-digit', day: 'numeric', year: 'numeric', hour: "numeric", minute: "2-digit" };
+    document.getElementsByClassName("datetimeDisplay")[0].innerHTML = simStartDate.toLocaleDateString('en-US', options);
+    let simulation = setInterval(simulationStep, 1000);
   }
 }
 
