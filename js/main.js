b1c979b3fe2a00a84edd0ad79c7f6017f473bde8
Daniel Hackney <corncob567dh@gmail.com>
2022-10-11 14:27:02 -0400

    Proof of concept for simulation.

--- js/main.js
+++ js/main.js
@@ -4,33 +4,33 @@ let isSimulationRunning = false;
 
 let inputContainer = document.querySelector(".input-container");
 inputContainer.addEventListener("click", changeTemp);
-let input = inputContainer.firstElementChild.nextElementSibling;
+let setTempInput = inputContainer.firstElementChild.nextElementSibling;
 let minus = inputContainer.firstElementChild;
 let plus = inputContainer.lastElementChild;
 
 function changeTemp(e) {
-  let temp = +input.value.substring(0, input.value.indexOf(' '));
-  let unit = ' ' + input.value.substring(input.value.indexOf(' ') + 1); // The unit for the temperature
+  let temp = +setTempInput.value.substring(0, setTempInput.value.indexOf(' '));
+  let unit = ' ' + setTempInput.value.substring(setTempInput.value.indexOf(' ') + 1); // The unit for the temperature
   if(e.target == minus && temp > MINTEMP) {
     temp--;
-    input.value = temp + unit;
+    setTempInput.value = temp + unit;
   } else if(e.target == plus && temp < MAXTEMP) {
     temp++;
-    input.value = temp + unit;
+    setTempInput.value = temp + unit;
   }
   if(temp == MINTEMP){
     minus.disabled = true;
   }
   else if(temp == MAXTEMP){
     plus.disabled = true;
-  }
+  } 
   else{
     plus.disabled = false;
     minus.disabled = false;
   }
 }
 
-updateClock() // Run this on load so the user sees the date/time immediately without having to wait the first second for it to update.
+updateClock(); // Run this on load so the user sees the date/time immediately without having to wait the first second for it to update.
 setInterval(updateClock, 1000);
 function updateClock() {
   if(isSimulationRunning == false){
@@ -40,22 +40,61 @@ function updateClock() {
   }
 }
 
+let simStartDate = new Date(Date.UTC(2022, 11, 1, 5, 0, 0));
+let simStepCount = 0
+let simDate = new Date(Date.UTC(2022, 11, 1, 5, 0, 0));
+let simDateOptions = { month: '2-digit', day: 'numeric', year: 'numeric', hour: "numeric", minute: "2-digit" };
+let simStartButton = document.getElementById("simStart");
+let simulation
+
 function startSimulation(){
   if (isSimulationRunning == false){
-    isSimulationRunning = true
-    let simStartButton = document.getElementById("simStart");
-    simStartButton.innerHTML = "Simulation Running"
-    simStartButton.disabled = 'true';
-    let simStartDate = new Date(Date.UTC(2022, 11, 1, 5, 0, 0));
-    let options = { month: '2-digit', day: 'numeric', year: 'numeric', hour: "numeric", minute: "2-digit" };
-    document.getElementsByClassName("datetimeDisplay")[0].innerHTML = simStartDate.toLocaleDateString('en-US', options);
-    let simulation = setInterval(simulationStep, 1000);
+    isSimulationRunning = true;
+    simStartButton.innerHTML = "Simulation Running";
+    simStartButton.disabled = true;
+    simDate = simStartDate;
+    simStepCount = 0;
+    document.getElementsByClassName("datetimeDisplay")[0].innerHTML = simStartDate.toLocaleDateString('en-US', simDateOptions);
+    simulation = setInterval(simulationStep, 10);
+  }
+}
+
+function addMinutes(date, minutes) {
+  simDate = new Date(date.getTime() + minutes * 60000);
+  document.getElementsByClassName("datetimeDisplay")[0].innerHTML = simDate.toLocaleDateString('en-US', simDateOptions);
+}
+
+function setTemp(temp) {
+  let unit = ' ' + setTempInput.value.substring(setTempInput.value.indexOf(' ') + 1); // The unit for the temperature
+  if(temp >= MINTEMP && temp <= MAXTEMP) {
+    setTempInput.value = temp + unit;
+  }
+  if(temp == MINTEMP){
+    minus.disabled = true;
+  }
+  else if(temp == MAXTEMP){
+    plus.disabled = true;
+  } 
+  else{
+    plus.disabled = false;
+    minus.disabled = false;
   }
 }
 
 function simulationStep(){
-  console.log("Simulation Step");
-  let drawer = document.getElementById("waterTemp");
-  drawer.value = Number(drawer.value) + 1;
-  changeTemp('1', drawer.value)
+  if(simStepCount == 1440){
+    console.log("Simulation finished!");
+    updateClock();
+    isSimulationRunning = false
+    simStartButton.disabled = false;
+    simStartButton.innerHTML = "Start 24hr Simulation";
+    clearInterval(simulation);
+  }else{
+    //console.log("Simulation Step " + simStepCount);
+    addMinutes(simDate, 1);
+    if(simStepCount == 1300){
+      setTemp(39);
+    }
+    simStepCount++;
+  }
 }
